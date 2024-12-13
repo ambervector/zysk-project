e40d4085b74e88ad38391652e6d1d4dd495a8999
Amber <amber.alam@dive.money>
2024-12-13 19:24:40 +0530

    Navbar Added and tested

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import BasicButtons from "./components/BasicButtons";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <BasicButtons />
     </div>
   );
